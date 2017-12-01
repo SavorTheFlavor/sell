cfa0634e4d9283b65c7c8ee355f6b90f19ad88a7
SavorTheFlavor2 <2500364530@qq.com>
2017-12-02 01:50:12 +0800

    product category ---- service
    都没写什么..

--- src/main/java/com/me/bean/ProductCategory.java
+++ src/main/java/com/me/bean/ProductCategory.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * Created by Administrator on 2017/11/29.
  */
 @Entity
-@DynamicUpdate //生成动态的update语句,让updateTime字段动态更新?
+@DynamicUpdate //为null的字段不会写到sql里进行更新，没有参与到update，则updateTime就可以被自动被赋值
 //@Table(name="product_category") springboot会自动转驼峰
 //@Data lombok, 自动为属性生成getter, setter, toString... 估计通过注解处理器API利用反射生成java代码来代替原来的吧
 public class ProductCategory {
---
+++ src/test/java/com/me/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.me.service.impl;
+
+import com.me.bean.ProductCategory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryServiceImplTest {
+
+    @Autowired
+    private CategoryServiceImpl categoryService;
+
+    @Test
+    public void findOne() throws Exception {
+        ProductCategory category = categoryService.findOne(1);
+        System.err.print(category);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+}
