6ab0fd306b345df95b96b009b08ee6d8dcf1424a
SavorTheFlavor2 <2500364530@qq.com>
2018-02-01 13:08:51 +0800

    异常捕获

--- src/main/java/com/me/sell/exception/SellException.java
+++ src/main/java/com/me/sell/exception/SellException.java
@@ -1,10 +1,12 @@
 package com.me.sell.exception;
 
 import com.me.sell.enums.ResultEnum;
+import lombok.Getter;
 
 /**
  * Created by Administrator on 2017/12/5.
  */
+@Getter
 public class SellException extends RuntimeException {
     private Integer code;
 
--- src/main/java/com/me/sell/handler/SellExceptionHandler.java
+++ src/main/java/com/me/sell/handler/SellExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.me.sell.handler;
 
 import com.me.sell.config.ProjectUrlConfig;
+import com.me.sell.exception.SellException;
 import com.me.sell.exception.SellerAuthorizeException;
+import com.me.sell.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -29,6 +34,13 @@ public class SellExceptionHandler {
         .concat(projectUrlConfig.getSell())
         .concat("/sell/seller/login"));
 
+    }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody  //返回ResultVO对象，但http状态是200
+    // @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) 直接给前端返回500
+    public ResultVO handleSellException(SellException e){
+        return ResultVO.error(e.getCode(),e.getMessage());
     }
 
 }
